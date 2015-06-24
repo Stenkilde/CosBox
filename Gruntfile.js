4d3ba649c104ba5b3c08a3fcc9f31e2828af60cd
Oliver Stenkilde <oliver@stenkilde.com>
2015-06-24 16:21:48 +0200

    Did a lot of work to the grunfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,57 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 
-		jshin: {
+		jshint: {
 			all: ['public/app/logic/**/*.js']
 		},
 
 		sass: {
-			dist: {
-				options: {
-					style: 'expanded'
+		    dist: {
+		    	options: {
+		    		style: 'expanded'
+		    	},
+		    	files: {
+		    		'public/app/styles/main.css': 'public/app/styles/main.scss',
+		    	}
+		    }
+		},
+
+		cssmin: {
+			build: {
+				files: {
+					'public/app/styles/style.min.css' : 'public/app/styles/style.css'
 				}
-			},
-			files: {
-				'main.css' : 'main.scss'
 			}
-		}
+		},
+
+		watch: {
+			css: {
+				files: ['public/app/styles/**/*.scss'],
+				tasks: ['sass']
+			}
+		},
 
 		nodemon: {
 			dev: {
 				script: 'server.js'
 			}
+		},
+
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			tasks: ['nodemon', 'watch']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-nodemon');
-	grunt.loadNpmTasks('grund-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-concurrent');
 
 
-	grunt.registerTask('default', ['nodemon']);
+	grunt.registerTask('default', ['sass', 'cssmin', 'jshint', 'concurrent']);
 };
